d61e5abe55ea7aed5522b060cfdecb56bc24cc7d
mnjammy007 <naseem4954r@gmail.com>
2024-10-16 15:47:05 +0530

    Initial Commit

---
+++ JPApexPredators18/JPApexPredators18App.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredators18App.swift
+//  JPApexPredators18
+//
+//  Created by Apple on 16/10/24.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredators18App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JPApexPredators18Tests/JPApexPredators18Tests.swift
@@ -0,0 +1,16 @@
+//
+//  JPApexPredators18Tests.swift
+//  JPApexPredators18Tests
+//
+//  Created by Apple on 16/10/24.
+//
+
+import Testing
+
+struct JPApexPredators18Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
